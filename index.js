386e6b9ee4b056668face829fd807613c8c18baf
entropy82 <froutopotos.alex@gmail.com>
2017-11-14 11:50:13 +0200

    fix the sqlite connection

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 
+
 let knex = require('knex')({
     client: "sqlite3",
     connection: {
-        filename: "./passcodes.db"
+        filename: "./passcodes.sqlite"
     }
 });
+// console.log(knex);
+
 
 app.on("ready", () => {
     let mainWindow = new BrowserWindow({ height: 800, width: 800, show: false })
     mainWindow.loadURL(`file://${__dirname}/main.html`);
     mainWindow.once("ready-to-show", () => {
         mainWindow.show();
+        console.log('window-loaded');
     })
 
-    ipcMain.on("mainWindowLoaded", () => {
-        let result = knex.select("name").from("apps")
+    ipcMain.on("mainWindowLoaded", function ()  {
+        console.log("pass!");
+        let result = knex.select("user").from("users")
         result.then(  (rows) => {
             mainWindow.webContents.send("resultSent", rows);
             console.log(rows);
+            console.log('promise resolved');
         })
+        console.log("promise started");
     })
 
 
---
+++ main.js
@@ -0,0 +1,12 @@
+const electron = require("electron");
+const ipc = electron.ipcRenderer;
+document.addEventListener("DOMContentLoaded", function(){
+    ipc.send("mainWindowLoaded")
+    ipc.on("resultSent", function(evt, result){
+        let resultEl = document.getElementById("result");
+        console.log(result);
+        for(var i = 0; i < result.length;i++){
+            resultEl.innerHTML += "First Name: " + result[i].user.toString() + "<br/>";
+        }
+    });
+});
