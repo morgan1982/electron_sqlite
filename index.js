ab9332606c1540c77cfbf0dd0c4d504d0f40d1c3
entropy82 <froutopotos.alex@gmail.com>
2017-11-14 13:22:05 +0200

    add menu second window garbage colllection

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, Menu } = require('electron');
+const url = require('url');
+const path = require('path');
 
 
 let knex = require('knex')({
@@ -17,6 +19,10 @@ app.on("ready", () => {
         mainWindow.show();
         console.log('window-loaded');
     })
+    //build the menu
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // insert the menu
+    Menu.setApplicationMenu(mainMenu);
 
     ipcMain.on("mainWindowLoaded", function ()  {
         console.log("pass!");
@@ -28,8 +34,54 @@ app.on("ready", () => {
         })
         console.log("promise started");
     })
+    mainWindow.on('closed', () => {
+        app.quit();
+    })
 
 
 })
+
+//Handle createAddWindow
+function createAddWindow() {
+    let addWindow = new BrowserWindow({
+        width: 200,
+        height: 300,
+        title: 'Add new record'
+    });
+    //load the html file
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    // garbage collection
+    addWindow.on('closed', () => {
+        addWindow = null;
+    })
+}
+
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Add record',
+                click() {
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Delete record'
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command + Q' : 'Ctrl + Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+]
 
 app.on("window-all-closed", () => { app.quit() });
